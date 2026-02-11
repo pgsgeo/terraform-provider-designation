1d1a4be67f2eb3cc88bce92871a846b64352c31f
Torbjørn Fjørtoft <torbjorn.fjortoft@tgs.com>
2026-02-11 08:59:11 +0100

    chore: let's see if we can build for both registries

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ func main() {
 
 	opts := providerserver.ServeOpts{
 		// TODO: Update this string with the published name of your provider.
-		Address: "search.opentofu.org/pgsgeo/designation",
+		Address: "registry.terraform.io/pgsgeo/designation",
 		Debug:   debug,
 	}
 
